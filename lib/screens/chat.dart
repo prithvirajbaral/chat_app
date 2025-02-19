c210564ef26c615ec02d6374451ca3cb1bb94954
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-19 17:43:07 +0530

    Subscribe to 'chat' topic for push notifications in setupPushNotifications method

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -15,8 +15,10 @@ class _ChatScreenState extends State<ChatScreen> {
   void setupPushNotifications() async {
     final fcm = FirebaseMessaging.instance;
     await fcm.requestPermission();
-    final token = await fcm.getToken();
-    print(token);
+    // final token = await fcm.getToken();
+    // print(token);
+
+    fcm.subscribeToTopic('chat');
   }
 
   @override
